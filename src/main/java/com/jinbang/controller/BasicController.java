eb7de63bfbb8650076c6f2fe4617fc9f0ec5508d
Joliven <joliven_d@qq.com>
2019-05-12 22:36:38 +0800

    1

--- src/main/java/com/jinbang/controller/BasicController.java
+++ src/main/java/com/jinbang/controller/BasicController.java
@@ -5,11 +5,15 @@ import com.jinbang.model.Item_Asr_Usr_IK_Kp;
 import com.jinbang.model.User;
 import com.jinbang.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -20,61 +24,46 @@ public class BasicController {
     ItemService itemService;
 
     @RequestMapping("/")
-    public ModelAndView cover(){
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("cover");
-        return mv;
+    public ResponseEntity<Map<String,Object>> cover(){
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("success", "index page");
+        return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
     }
-//    @RequestMapping("/")
-//    public String cover(){
-//        return "index";
-//    }
-    @GetMapping ("/signin")
-    public ModelAndView signin(){
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("signin");
-        mv.addObject("user", new User());
-        return mv;
-    }
-
     @PostMapping("/home")
-    public ModelAndView signinPost(User user, HttpSession session){
-        ModelAndView modelAndView = new ModelAndView();
-        User userDB = userMapper.loadByUserName(user.getName());
+    public ResponseEntity<Map<String,Object>> signinPost(HttpServletRequest request, HttpSession session){
+        Map<String, Object> map = new HashMap<String, Object>();
+        String name = request.getParameter("name");
+        User userDB = userMapper.loadByUserName(name);
         if(userDB != null){
 //            String pwdDB = DigestUtils.md5DigestAsHex(userDB.getPwd().getBytes());
             String pwdDB = userDB.getPwd();
 //            System.out.println("user.getPwd() " + user.getPwd());
 //            System.out.println("pwdDB " + pwdDB);
-            if(pwdDB.equals(user.getPwd())){
-                System.out.println(user.getName() + " 密码正确登录成功");
+            if(pwdDB.equals(request.getParameter("pwd"))){
+                System.out.println(name + " 密码正确登录成功");
                 // 写入 session
-                session.setAttribute("name", user.getName());
-                modelAndView.setViewName("home");
-                modelAndView.addObject("name", user.getName());
-//                List<Item_Asr_Usr_IK_Kp> item_asr_usr_ik_kps;
-//                item_asr_usr_ik_kps = itemService.getItem_Asr_Usr_IK_KpAll();
-//                modelAndView.addObject("items", item_asr_usr_ik_kps);
-            } else{
+                session.setAttribute("name", name);
+            } else {
                 System.out.println("密码错误");
-                modelAndView.setViewName("relogin");
-                modelAndView.addObject("user", new User());
+                map.put("err", "Wrong pwd!");
+                return new ResponseEntity<Map<String,Object>>(map, HttpStatus.BAD_REQUEST);
             }
         } else {
-            System.out.println(user.getName() + " 不存在");
-            modelAndView.setViewName("relogin");
-            modelAndView.addObject("user", new User());
+            System.out.println(name + " 不存在");
+            map.put("err", "Name not exists!");
+            return new ResponseEntity<Map<String,Object>>(map, HttpStatus.BAD_REQUEST);
         }
-        return modelAndView;
+        map.put("seccess", "Login successful!");
+        return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
     }
     @RequestMapping("/signout")
-    public ModelAndView signout(HttpSession session){
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("cover");
+    public ResponseEntity<Map<String,Object>> signout(HttpSession session){
+        Map<String, Object> map = new HashMap<String, Object>();
         // 取出 session 中的 name
         Object name = session.getAttribute("name");
         System.out.println(name.toString() + "注销！");
         session.removeAttribute("name");
-        return mv;
+        map.put("success", "Logged out!");
+        return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
     }
 }
--- src/main/java/com/jinbang/controller/ItemController.java
+++ src/main/java/com/jinbang/controller/ItemController.java
@@ -1,5 +1,6 @@
 package com.jinbang.controller;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.jinbang.mapper.ItemMapper;
 import com.jinbang.mapper.UserMapper;
 import com.jinbang.model.Item;
@@ -9,12 +10,18 @@ import com.jinbang.service.ItemService;
 import javafx.beans.binding.ObjectExpression;
 import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "*",
         maxAge = 3600,
@@ -29,24 +36,45 @@ public class ItemController {
     UserMapper userMapper;
 
     @GetMapping("/itemall")
-    public List<Item_Asr_Usr_IK_Kp> itemall(){
-        List<Item_Asr_Usr_IK_Kp> item_asr_usr_ik_kps;
-        item_asr_usr_ik_kps = itemService.itemall();
-        return item_asr_usr_ik_kps;
+    public ResponseEntity<Map<String,Object>> itemall(HttpSession session) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        if(session.getAttribute("name")!=null){
+            List<Item_Asr_Usr_IK_Kp> item_asr_usr_ik_kps;
+            item_asr_usr_ik_kps = itemService.itemall();
+            map.put("itemall", item_asr_usr_ik_kps);
+            return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
+        } else {
+            map.put("err", "Not Logged!");
+            return new ResponseEntity<Map<String,Object>>(map, HttpStatus.BAD_REQUEST);
+        }
     }
     @GetMapping("/itemradio")
-    public List<HashMap> itemradio(){
-        List<HashMap> itemlist = itemService.itemradio();
-        return itemlist;
+    public ResponseEntity<Map<String,Object>> itemradio(HttpSession session){
+        Map<String, Object> map = new HashMap<String, Object>();
+        if(session.getAttribute("name")!=null){
+            List<HashMap> itemlist = itemService.itemradio();
+            map.put("itemradio", itemlist);
+            return new ResponseEntity<>(map, HttpStatus.OK);
+        } else {
+            map.put("err", "Not Logged!");
+            return new ResponseEntity<Map<String,Object>>(map, HttpStatus.BAD_REQUEST);
+        }
     }
     @PostMapping("/itemchoose")
-    public List<Item_Asr_Usr_IK_Kp> itemchoose(HttpServletRequest request){
-        String type = request.getParameter("type");
-        String grade = request.getParameter("grade");
-        String source = request.getParameter("source");
-        String name = request.getParameter("name");
+    public ResponseEntity<Map<String,Object>> itemchoose(HttpServletRequest request, HttpSession session) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        if(session.getAttribute("name")!=null){
+            String type = request.getParameter("type");
+            String grade = request.getParameter("grade");
+            String source = request.getParameter("source");
+            String name = request.getParameter("name");
 //        System.out.println("type: " + type + ", grade: " + grade + ", source: " + source + ", name: " + name);
-        List<Item_Asr_Usr_IK_Kp> item_asr_usr_ik_kps = itemService.itemchoose(type, grade, source, name);
-        return item_asr_usr_ik_kps;
+            List<Item_Asr_Usr_IK_Kp> item_asr_usr_ik_kps = itemService.itemchoose(type, grade, source, name);
+            map.put("itemchoose", item_asr_usr_ik_kps);
+            return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
+        } else {
+            map.put("err", "Not Logged!");
+            return new ResponseEntity<Map<String,Object>>(map, HttpStatus.BAD_REQUEST);
+        }
     }
 }
