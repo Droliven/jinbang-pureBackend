95da4d5d779a5fee0153973703b6f8bd09a0631f
Joliven <joliven_d@163.com>
2019-07-07 23:29:04 +0800

    登录与权限实现但还有500错误

--- src/main/java/com/jinbang/controller/ItemController.java
+++ src/main/java/com/jinbang/controller/ItemController.java
@@ -13,9 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class ItemController {
@@ -33,18 +31,29 @@ public class ItemController {
     @PostMapping("/itemradio")
     public Map<String,Object> itemradio(@RequestBody JSONObject request, HttpSession session){
         Map<String, Object> map = new HashMap<String, Object>();
+
         String clientsession = request.get("session").toString();
+
+        JSONObject data = JSON.parseObject(request.get("data").toString());
+        JSONObject userdetail = data.getJSONObject("userdetail");
+        Set<String> rscs = new HashSet<>(JSON.parseArray(userdetail.get("shiroResources").toString(), String.class));
+
         String name = clientsession.split(",")[0];
         if(session.getAttribute(name) != null){
-            // 业务代码
-            System.out.println("itemradio");
-            List<HashMap> itemlist = itemService.itemradio();
+            if(rscs.contains("itemradio")) {
+                // 业务代码
+                System.out.println("itemradio");
+                List<HashMap> itemlist = itemService.itemradio();
 
-            String serverSession = name + "," + System.currentTimeMillis();
-            session.setAttribute(name, serverSession);
-            map.put("data", itemlist);
-            map.put("state", "success");
-            map.put("session", serverSession);
+                String serverSession = name + "," + System.currentTimeMillis();
+                session.setAttribute(name, serverSession);
+                map.put("data", itemlist);
+                map.put("state", "success");
+                map.put("session", serverSession);
+            } else {
+                map.put("state", "err");
+                map.put("msg", "没有权限");
+            }
         } else {
             map.put("state", "err");
             map.put("msg", "未登录");
@@ -55,42 +64,56 @@ public class ItemController {
     @PostMapping("/itemchoose")
     public Map<String,Object> itemchoose(@RequestBody JSONObject request, HttpSession session){
         Map<String, Object> map = new HashMap<String, Object>();
-        JSONObject data = JSON.parseObject(request.get("data").toString());
-        String clientsession = request.get("session").toString();
-        String name = clientsession.split(",")[0];
-        if(session.getAttribute(name) != null){
-            System.out.println("itemchoose");
 
-            String type = "", grade = "", source = "", dataname = "";
-            if(data.get("type") != null){
-                type = data.get("type").toString();
-            }
-            if(data.get("grade") != null){
-                grade = data.get("grade").toString();
-            }
-            if(data.get("source") != null){
-                source = data.get("source").toString();
-            }
-            if(data.get("name") != null){
-                dataname = data.get("name").toString();
-            }
-            if(!(data.get("path") == null || data.get("path").equals(""))){
-                String path = data.get("path").toString();
-                System.out.println("controller path: " + path);
-                String [] pathssplit = path.split("/");
-                String kp = pathssplit[pathssplit.length-1];
-                System.out.println("controller kp: " + kp);
-                List<Item_Asr_Usr_IK_Kp> item_asr_usr_ik_kps = itemService.itemchoose(type, grade, source, dataname, kp);
-                map.put("data", item_asr_usr_ik_kps);
+        String clientsession = request.get("session").toString();
+
+        JSONObject data = JSON.parseObject(request.get("data").toString());
+        JSONObject userdetail = data.getJSONObject("userdetail");
+        Set<String> rscs = new HashSet<>(JSON.parseArray(userdetail.get("shiroResources").toString(), String.class));
+
+        JSONObject realdata = data.getJSONObject("data");
+
+        String name = clientsession.split(",")[0];
+
+        if(session.getAttribute(name) != null) {
+            if(rscs.contains("itemchoose")) {
+                // 业务代码
+                System.out.println("itemchoose");
+
+                String type = "", grade = "", source = "", dataname = "";
+                if(realdata.get("type") != null){
+                    type = realdata.get("type").toString();
+                }
+                if(realdata.get("grade") != null){
+                    grade = realdata.get("grade").toString();
+                }
+                if(realdata.get("source") != null){
+                    source = realdata.get("source").toString();
+                }
+                if(realdata.get("name") != null){
+                    dataname = realdata.get("name").toString();
+                }
+                if(!(realdata.get("path") == null || realdata.get("path").equals(""))){
+                    String path = realdata.get("path").toString();
+                    System.out.println("controller path: " + path);
+                    String [] pathssplit = path.split("/");
+                    String kp = pathssplit[pathssplit.length-1];
+                    System.out.println("controller kp: " + kp);
+                    List<Item_Asr_Usr_IK_Kp> item_asr_usr_ik_kps = itemService.itemchoose(type, grade, source, dataname, kp);
+                    map.put("data", item_asr_usr_ik_kps);
+                } else {
+                    String path = "";
+                    List<Item_Asr_Usr_IK_Kp> item_asr_usr_ik_kps = itemService.itemchoose(type, grade, source, name, path);
+                    map.put("data", item_asr_usr_ik_kps);
+                }
+                String serverSession = name + "," + System.currentTimeMillis();
+                session.setAttribute(name, serverSession);
+                map.put("state", "success");
+                map.put("session", serverSession);
             } else {
-                String path = "";
-                List<Item_Asr_Usr_IK_Kp> item_asr_usr_ik_kps = itemService.itemchoose(type, grade, source, name, path);
-                map.put("data", item_asr_usr_ik_kps);
+                map.put("state", "err");
+                map.put("msg", "没有权限");
             }
-            String serverSession = name + "," + System.currentTimeMillis();
-            session.setAttribute(name, serverSession);
-            map.put("state", "success");
-            map.put("session", serverSession);
         } else {
             map.put("state", "err");
             map.put("msg", "未登录");
@@ -101,19 +124,31 @@ public class ItemController {
     @PostMapping ("/itemDeleteByIids")
     public Map<String,Object> itemDeleteByIids(@RequestBody JSONObject request, HttpSession session){
         Map<String, Object> map = new HashMap<String, Object>();
-        JSONObject data = JSON.parseObject(request.get("data").toString());
+
         String clientsession = request.get("session").toString();
+
+        JSONObject data = JSON.parseObject(request.get("data").toString());
+        JSONObject userdetail = data.getJSONObject("userdetail");
+        Set<String> rscs = new HashSet<>(JSON.parseArray(userdetail.get("shiroResources").toString(), String.class));
+
+        JSONObject realdata = data.getJSONObject("data");
+
         String name = clientsession.split(",")[0];
         if(session.getAttribute(name) != null){
-            System.out.println("itemDeleteByIids");
-            JSONArray reqArray = JSON.parseArray(data.get("iids").toString());
-            JSONObject jsonObject = itemService.itemDeleteByIids(reqArray);
+            if(rscs.contains("itemDeleteByIids")) {
+                System.out.println("itemDeleteByIids");
+                JSONArray reqArray = JSON.parseArray(data.get("iids").toString());
+                JSONObject jsonObject = itemService.itemDeleteByIids(reqArray);
 
-            String serverSession = name + "," + System.currentTimeMillis();
-            session.setAttribute(name, serverSession);
-            map.put("data", jsonObject);
-            map.put("state", "success");
-            map.put("session", serverSession);
+                String serverSession = name + "," + System.currentTimeMillis();
+                session.setAttribute(name, serverSession);
+                map.put("data", jsonObject);
+                map.put("state", "success");
+                map.put("session", serverSession);
+            } else {
+                map.put("state", "err");
+                map.put("msg", "没有权限");
+            }
         } else {
             map.put("state", "err");
             map.put("msg", "未登录");
@@ -124,19 +159,33 @@ public class ItemController {
     @PostMapping("/getRestBranch")
     public Map<String,Object> getRestBranch(@RequestBody JSONObject request, HttpSession session){
         Map<String, Object> map = new HashMap<String, Object>();
-        JSONObject data = JSON.parseObject(request.get("data").toString());
-        String clientsession = request.get("session").toString();
-        String name = clientsession.split(",")[0];
-        if(session.getAttribute(name) != null){
-            String node = data.get("node").toString();
-            JSONArray jsonArray = kpPathService.getRestBranch(node);
-            System.out.println("getRestBranch");
 
-            String serverSession = name + "," + System.currentTimeMillis();
-            session.setAttribute(name, serverSession);
-            map.put("data", jsonArray);
-            map.put("state", "success");
-            map.put("session", serverSession);
+        String clientsession = request.get("session").toString();
+
+        JSONObject data = JSON.parseObject(request.get("data").toString());
+        JSONObject userdetail = data.getJSONObject("userdetail");
+        Set<String> rscs = new HashSet<>(JSON.parseArray(userdetail.get("shiroResources").toString(), String.class));
+
+        JSONObject realdata = data.getJSONObject("data");
+
+        String name = clientsession.split(",")[0];
+
+        if(session.getAttribute(name) != null){
+            if(rscs.contains("getRestBranch")) {
+                // 业务代码
+                String node = data.get("node").toString();
+                JSONArray jsonArray = kpPathService.getRestBranch(node);
+                System.out.println("getRestBranch");
+
+                String serverSession = name + "," + System.currentTimeMillis();
+                session.setAttribute(name, serverSession);
+                map.put("data", jsonArray);
+                map.put("state", "success");
+                map.put("session", serverSession);
+            } else {
+                map.put("state", "err");
+                map.put("msg", "没有权限");
+            }
         } else {
             map.put("state", "err");
             map.put("msg", "未登录");
@@ -147,19 +196,32 @@ public class ItemController {
     @PostMapping("/addKpByPath")
     public Map<String,Object> addKpByPath(@RequestBody JSONObject request, HttpSession session) {
         Map<String, Object> map = new HashMap<String, Object>();
-        JSONObject data = JSON.parseObject(request.get("data").toString());
+
         String clientsession = request.get("session").toString();
+
+        JSONObject data = JSON.parseObject(request.get("data").toString());
+        JSONObject userdetail = data.getJSONObject("userdetail");
+        Set<String> rscs = new HashSet<>(JSON.parseArray(userdetail.get("shiroResources").toString(), String.class));
+
+        JSONObject realdata = data.getJSONObject("data");
+
         String name = clientsession.split(",")[0];
         if(session.getAttribute(name) != null){
-            String path = data.get("path").toString();;
-            System.out.println("path: " + path);
-            kpPathService.addKpByPath(path);
-            System.out.println("addKpByPath");
+            if(rscs.contains("addKpByPath")) {
+                // 业务代码
+                String path = data.get("path").toString();;
+                System.out.println("path: " + path);
+                kpPathService.addKpByPath(path);
+                System.out.println("addKpByPath");
 
-            String serverSession = name + "," + System.currentTimeMillis();
-            session.setAttribute(name, serverSession);
-            map.put("state", "success");
-            map.put("session", serverSession);
+                String serverSession = name + "," + System.currentTimeMillis();
+                session.setAttribute(name, serverSession);
+                map.put("state", "success");
+                map.put("session", serverSession);
+            } else {
+                map.put("state", "err");
+                map.put("msg", "没有权限");
+            }
         } else {
             map.put("state", "err");
             map.put("msg", "未登录");
@@ -170,17 +232,30 @@ public class ItemController {
     @PostMapping ("/editItemFully")
     public Map<String,Object> editItemFully(@RequestBody JSONObject request, HttpSession session){
         Map<String, Object> map = new HashMap<String, Object>();
-        JSONObject data = JSON.parseObject(request.get("data").toString());
+
         String clientsession = request.get("session").toString();
+
+        JSONObject data = JSON.parseObject(request.get("data").toString());
+        JSONObject userdetail = data.getJSONObject("userdetail");
+        Set<String> rscs = new HashSet<>(JSON.parseArray(userdetail.get("shiroResources").toString(), String.class));
+
+        JSONObject realdata = data.getJSONObject("data");
+
         String name = clientsession.split(",")[0];
         if(session.getAttribute(name) != null){
-            itemService.editItemFully(data);
-            System.out.println("editItemFully");
+            if(rscs.contains("editItemFully")) {
+                // 业务代码
+                itemService.editItemFully(data);
+                System.out.println("editItemFully");
 
-            String serverSession = name + "," + System.currentTimeMillis();
-            session.setAttribute(name, serverSession);
-            map.put("state", "success");
-            map.put("session", serverSession);
+                String serverSession = name + "," + System.currentTimeMillis();
+                session.setAttribute(name, serverSession);
+                map.put("state", "success");
+                map.put("session", serverSession);
+            } else {
+                map.put("state", "err");
+                map.put("msg", "没有权限");
+            }
         } else {
             map.put("state", "err");
             map.put("msg", "未登录");
@@ -191,17 +266,30 @@ public class ItemController {
     @PostMapping("/addItemFully")
     public Map<String,Object> addItemFully(@RequestBody JSONObject request, HttpSession session) {
         Map<String, Object> map = new HashMap<String, Object>();
-        JSONObject data = JSON.parseObject(request.get("data").toString());
-        String clientsession = request.get("session").toString();
-        String name = clientsession.split(",")[0];
-        if (session.getAttribute(name) != null) {
-            System.out.println("addItemFully");
-            itemService.addItemFully(data);
 
-            String serverSession = name + "," + System.currentTimeMillis();
-            session.setAttribute(name, serverSession);
-            map.put("state", "success");
-            map.put("session", serverSession);
+        String clientsession = request.get("session").toString();
+
+        JSONObject data = JSON.parseObject(request.get("data").toString());
+        JSONObject userdetail = data.getJSONObject("userdetail");
+        Set<String> rscs = new HashSet<>(JSON.parseArray(userdetail.get("shiroResources").toString(), String.class));
+
+        JSONObject realdata = data.getJSONObject("data");
+
+        String name = clientsession.split(",")[0];
+        if(session.getAttribute(name) != null){
+            if(rscs.contains("addItemFully")) {
+                // 业务代码
+                System.out.println("addItemFully");
+                itemService.addItemFully(data);
+
+                String serverSession = name + "," + System.currentTimeMillis();
+                session.setAttribute(name, serverSession);
+                map.put("state", "success");
+                map.put("session", serverSession);
+            } else {
+                map.put("state", "err");
+                map.put("msg", "没有权限");
+            }
         } else {
             map.put("state", "err");
             map.put("msg", "未登录");
--- src/main/java/com/jinbang/controller/PaperController.java
+++ src/main/java/com/jinbang/controller/PaperController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 @Controller
 public class PaperController {
@@ -22,18 +24,31 @@ public class PaperController {
     @PostMapping("/createEmptyPaper")
     public Map<String, Object> createEmptyPaper(@RequestBody JSONObject request, HttpSession session){
         Map<String, Object> map = new HashMap<String, Object>();
-        JSONObject data = JSON.parseObject(request.get("data").toString());
+
         String clientsession = request.get("session").toString();
+
+        JSONObject data = JSON.parseObject(request.get("data").toString());
+        JSONObject userdetail = data.getJSONObject("userdetail");
+        Set<String> rscs = new HashSet<>(JSON.parseArray(userdetail.get("shiroResources").toString(), String.class));
+
+        JSONObject realdata = data.getJSONObject("data");
+
         String name = clientsession.split(",")[0];
         if(session.getAttribute(name) != null){
-            int pid = paperService.createEmptyPaper(data);
-            System.out.println("createEmptyPaper");
+            if(rscs.contains("itemradio")) {
+                // 业务代码
+                int pid = paperService.createEmptyPaper(realdata);
+                System.out.println("createEmptyPaper");
 
-            String serverSession = name + "," + System.currentTimeMillis();
-            session.setAttribute(name, serverSession);
-            map.put("data", pid);
-            map.put("state", "success");
-            map.put("session", serverSession);
+                String serverSession = name + "," + System.currentTimeMillis();
+                session.setAttribute(name, serverSession);
+                map.put("data", pid);
+                map.put("state", "success");
+                map.put("session", serverSession);
+            } else {
+                map.put("state", "err");
+                map.put("msg", "没有权限");
+            }
         } else {
             map.put("state", "err");
             map.put("msg", "未登录");
@@ -44,17 +59,30 @@ public class PaperController {
     @PostMapping("/buildPaper")
     public Map<String, Object> buildPaper(@RequestBody JSONObject request, HttpSession session){
         Map<String, Object> map = new HashMap<String, Object>();
-        JSONObject data = JSON.parseObject(request.get("data").toString());
+
         String clientsession = request.get("session").toString();
+
+        JSONObject data = JSON.parseObject(request.get("data").toString());
+        JSONObject userdetail = data.getJSONObject("userdetail");
+        Set<String> rscs = new HashSet<>(JSON.parseArray(userdetail.get("shiroResources").toString(), String.class));
+
+        JSONObject realdata = data.getJSONObject("data");
+
         String name = clientsession.split(",")[0];
         if(session.getAttribute(name) != null){
-            paperService.buildPaper(data);
-            System.out.println("buildPaper");
+            if(rscs.contains("itemradio")) {
+                // 业务代码
+                paperService.buildPaper(data);
+                System.out.println("buildPaper");
 
-            String serverSession = name + "," + System.currentTimeMillis();
-            session.setAttribute(name, serverSession);
-            map.put("state", "success");
-            map.put("session", serverSession);
+                String serverSession = name + "," + System.currentTimeMillis();
+                session.setAttribute(name, serverSession);
+                map.put("state", "success");
+                map.put("session", serverSession);
+            } else {
+                map.put("state", "err");
+                map.put("msg", "没有权限");
+            }
         } else {
             map.put("state", "err");
             map.put("msg", "未登录");
@@ -64,20 +92,32 @@ public class PaperController {
 
     @PostMapping("/getAllPaperDetail")
     public Map<String, Object> getAllPaperDetail(@RequestBody JSONObject request, HttpSession session){
-        System.out.println("查看试卷");
         Map<String, Object> map = new HashMap<String, Object>();
-        JSONObject data = JSON.parseObject(request.get("data").toString());
+
         String clientsession = request.get("session").toString();
+
+        JSONObject data = JSON.parseObject(request.get("data").toString());
+        JSONObject userdetail = data.getJSONObject("userdetail");
+        Set<String> rscs = new HashSet<>(JSON.parseArray(userdetail.get("shiroResources").toString(), String.class));
+
+//        JSONObject realdata = data.getJSONObject("data");
+
         String name = clientsession.split(",")[0];
         if(session.getAttribute(name) != null){
-            System.out.println("getAllPaperDetail");
-            JSONArray allPaperAndBuildpapers = paperService.getAllPaperDetail();
+            if(rscs.contains("itemradio")) {
+                // 业务代码
+                System.out.println("getAllPaperDetail");
+                JSONArray allPaperAndBuildpapers = paperService.getAllPaperDetail();
 
-            String serverSession = name + "," + System.currentTimeMillis();
-            session.setAttribute(name, serverSession);
-            map.put("data", allPaperAndBuildpapers);
-            map.put("state", "success");
-            map.put("session", serverSession);
+                String serverSession = name + "," + System.currentTimeMillis();
+                session.setAttribute(name, serverSession);
+                map.put("data", allPaperAndBuildpapers);
+                map.put("state", "success");
+                map.put("session", serverSession);
+            } else {
+                map.put("state", "err");
+                map.put("msg", "没有权限");
+            }
         } else {
             map.put("state", "err");
             map.put("msg", "未登录");
